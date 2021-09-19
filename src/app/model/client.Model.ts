a3620685723d0394ba94b1071e82cfadcf754633
Dustman001 <dustman01@gmail.com>
2021-09-19 08:33:13 -0400

    updated models

--- src/app/model/client.Model.ts
+++ src/app/model/client.Model.ts
@@ -1,2 +1,14 @@
 export class Client {
+  id:number;
+  name:string;
+  birthdate:string;
+  contact:string;
+
+  constructor()
+  {
+    this.id=0;
+    this.name= "";
+    this.birthdate="";
+    this.contact="";
+  }
 }
--- src/app/model/loan.Model.ts
+++ src/app/model/loan.Model.ts
@@ -1,2 +1,14 @@
 export class Loan {
+  id:number;
+  amount:number;
+  date:string;
+  clientid:number;
+
+  constructor()
+  {
+    this.id=0;
+    this.amount=0;
+    this.date="";
+    this.clientid=0;
+  }
 }
